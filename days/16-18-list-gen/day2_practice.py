499d5154456211c32d7864c16b4871d72c44db73
Johnathan Staverman (AU) <johnathan.staverman@didata.com.au>
2019-06-23 23:52:28 +1000

    Bobs solution

--- days/16-18-list-gen/day2_practice.py
+++ days/16-18-list-gen/day2_practice.py
@@ -20,15 +20,18 @@ print(names2)
 single_names2 = [name.split()[0].title() for name in NAMES]
 print(f'Single Names List is: {single_names2}')
 
+item1, item2 = ["one", "two"]
+print(f'{item1}, {item2}')
+
 
 def gen_pairs():
     single_names = [name.split()[0].title() for name in NAMES]
-    dual_names = [single_names.pop(random.randrange(0, len(single_names) - 1)),
-                  single_names.pop(random.randrange(0, len(single_names) - 1))]
+    while True:
 
-    for pair in single_names:
-        yield f"{single_names.pop(random.randrange(0, len(single_names) - 1))} teams up with " \
-            f"{single_names.pop(random.randrange(0, len(single_names) - 1))}"
+        first, second = None, None
+        while first == second:
+            first, second = random.sample(single_names, 2)
+        yield f"{first} teams up with {second}"
 
 
 pairs = gen_pairs()
