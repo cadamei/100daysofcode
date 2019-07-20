06eef7bcd29a91556f582f6de6d64e384030eb81
Johnathan Staverman (AU) <johnathan.staverman@didata.com.au>
2019-07-20 21:20:20 +1000

    finished

--- days/37-39-csv/program.py
+++ days/37-39-csv/program.py
@@ -1,6 +1,15 @@
 import research
 
+
 # data from https://www.data.brisbane.qld.gov.au/data/dataset/food-safety-complaints
+
+def main():
+    research.init()
+
+    print("Safest 5 suburbs to eat out")
+    print(research.data)
+    print(research.safest_suburbs())
+    print(research.sickest_suburbs())
 
 
 if __name__ == '__main__':
--- days/37-39-csv/research.py
+++ days/37-39-csv/research.py
@@ -1,12 +1,47 @@
 import os
 import csv
+from collections import defaultdict, namedtuple, Counter, deque
 
 data = []
+
+Record = namedtuple(
+    'Record',
+    'nature,type,suburb'
+)
 
 
 def init():
     base_folder = os.path.dirname(__file__)
-    filename = os.path.join(base_folder, 'data', 'seattle.csv')
+    filename = os.path.join(base_folder, 'merged_data.csv')
 
     with open(filename, 'r', encoding='utf-8') as fin:
         reader = csv.DictReader(fin)
+
+        data.clear()
+        for row in reader:
+            record = parse_row(row)
+            data.append(record)
+
+
+def parse_row(row):
+    record = Record(
+        **row
+    )
+
+    return record
+
+
+def safest_suburbs():
+    suburbs = []
+    for record in data:
+        suburbs.append(record.suburb)
+
+    return Counter(suburbs).most_common()[:-5-1:-1]
+
+
+def sickest_suburbs():
+    suburbs = []
+    for record in data:
+        suburbs.append(record.suburb)
+
+    return Counter(suburbs).most_common(5)
