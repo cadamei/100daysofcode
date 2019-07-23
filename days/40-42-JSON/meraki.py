b6f814ccc327fba9633c70060eda33f2ea6aefeb
Johnathan Staverman (AU) <johnathan.staverman@didata.com.au>
2019-07-23 22:43:16 +1000

    added meraki request for organisations

--- days/40-42-JSON/meraki.py
+++ days/40-42-JSON/meraki.py
@@ -1,7 +1,14 @@
 #!python3
 
+
 import requests
 import json
 from pprint import pprint
 
+api_key = input("Please enter the API authorization token for Meraki:\n")
 
+sf_url = 'https://api.meraki.com/api/v0/organizations'
+
+r = requests.get(sf_url, headers={'X-Cisco-Meraki-API-Key': api_key, 'Content-Type': 'application/json'})
+
+pprint(r.json())
